8c0da0f80a0c42ad5e6ad36fecb81796cdd1ca83
Chris Parnin <chris.parnin@gmail.com>
2020-09-15 21:38:05 -0400

    initial version.

---
+++ index.js
@@ -0,0 +1,24 @@
+#! /usr/bin/env node
+
+console.log("Hello")
+
+var os = require('os');
+var pty = require('node-pty');
+
+var shell = os.platform() === 'win32' ? 'powershell.exe' : 'bash';
+
+var ptyProcess = pty.spawn(shell, [], {
+  name: 'xterm-color',
+  cols: 80,
+  rows: 30,
+  cwd: process.env.HOME,
+  env: process.env
+});
+
+ptyProcess.on('data', function(data) {
+  process.stdout.write(data);
+});
+
+ptyProcess.resize(100, 40);
+ptyProcess.write('opunit profile CSC-DevOps/profile:basics.yml\r');
+ptyProcess.write('exit\r');
